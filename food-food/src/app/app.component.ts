9952b9c6f027c5871d5df656ad42895aef426174
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-02-24 03:16:14 -0800

    feat:Adding a simple navigation using child to parent communitcation through event binding.

--- food-food/src/app/app.component.ts
+++ food-food/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'food-food';
+export class AppComponent{
+  loadedFeature ='recipe';
+  title = 'food-food'
+
+  onNavigate(feature:string){
+    this.loadedFeature =feature;
+  }
+
+  isRecipe():boolean{
+    return 'recipe' === this.loadedFeature;
+  }
+
+  isShoppingList():boolean{
+    return 'shopping-list' === this.loadedFeature;
+  }
 }
--- food-food/src/app/header/header.component.ts
+++ food-food/src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector:'app-header',
     templateUrl:'./header.component.html'
 })
 export class HeaderComponent{
-
+    @Output('featureEvent') selectFeatureEvent = new EventEmitter<string>();
+  
+    selectFeature(feature:string){
+        this.selectFeatureEvent.emit(feature);
+    }
 }
